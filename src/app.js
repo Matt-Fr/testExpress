69424efc5b3c2a51681a26dd24a4cb7df1bebbc9
Matt-Fr <matthieu_franck@orange.fr>
2023-08-02 09:47:44 -0400

    changed routes

--- src/app.js
+++ src/app.js
@@ -1,53 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
-
-app.listen(PORT, () => console.log(`running express server on port ${PORT}`));
+const groceriesRoute = require("./routes/grocery");
 
 //allows to send json to the server
 app.use(express.json());
 //allows to send urlencoded data
 app.use(express.urlencoded());
 
-const groceryList = [
-  {
-    item: "milk",
-    quantity: 2,
-  },
-  {
-    item: "steak",
-    quantity: 5,
-  },
-  {
-    item: "pop tart",
-    quantity: 3,
-  },
-];
-
 //a middleware is a funtion to be invoked in the middle of 2 main functionnalities (most common : login)
 
-//it allows client to get ressources
-app.get("/groceries", (requestObject, responseObject) => {
-  responseObject.send(groceryList);
-});
+app.use("/api/v1", groceriesRoute);
 
-//allow to create a ressource
-app.post("/groceries", (request, response) => {
-  // log what the client sent
-  console.log(request.body);
-  // send a status
-  response.sendStatus(201);
-  //push what send the client into our grocery list
-  groceryList.push(request.body);
-});
-
-app.get("/groceries/:item", (request, response) => {
-  //every single route params is gonna be stored
-  console.log(request.params.item);
-  // request.params.itema;
-  const { item } = request.params;
-  // find the item in the list
-  const groceryItem = groceryList.find((g) => g.item === item);
-  response.send(groceryItem);
-  s;
-});
+app.listen(PORT, () => console.log(`running express server on port ${PORT}`));
--- src/routes/grocery.js
+++ src/routes/grocery.js
@@ -1,3 +1,46 @@
 const { Router } = require("express");
 
 const router = Router();
+
+const groceryList = [
+  {
+    item: "milk",
+    quantity: 2,
+  },
+  {
+    item: "steak",
+    quantity: 5,
+  },
+  {
+    item: "pop tart",
+    quantity: 3,
+  },
+];
+
+//it allows client to get ressources
+router.get("/groceries", (requestObject, responseObject) => {
+  responseObject.send(groceryList);
+});
+
+//allow to create a ressource
+router.post("/groceries", (request, response) => {
+  // log what the client sent
+  console.log(request.body);
+  // send a status
+  response.sendStatus(201);
+  //push what send the client into our grocery list
+  groceryList.push(request.body);
+});
+
+router.get("/groceries/:item", (request, response) => {
+  //every single route params is gonna be stored
+  console.log(request.params.item);
+  // request.params.itema;
+  const { item } = request.params;
+  // find the item in the list
+  const groceryItem = groceryList.find((g) => g.item === item);
+  response.send(groceryItem);
+  s;
+});
+
+module.exports = router;
