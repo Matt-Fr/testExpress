24736489db1d8477d98e218942f5cdf6eaaad9cc
Matt-Fr <matthieu_franck@orange.fr>
2023-08-15 23:32:19 +0200

    add mongoose

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3001;
 const groceriesRoute = require("./routes/grocery");
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use(cookieParser());
+
+mongoo;
 
 app.use(
   session({
